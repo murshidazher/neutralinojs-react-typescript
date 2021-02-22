b4c5d111f4d6c4ca9c4ed3511c8ecd4555ac0a9c
murshidazher <murshidazher@yahoo.com>
2021-02-22 15:06:07 +0530

    :wrench: chore: update the author info

--- src/components/NeuDefault.tsx
+++ src/components/NeuDefault.tsx
@@ -4,7 +4,7 @@ import { Styled } from 'theme-ui';
 const NeuDefault = (): JSX.Element => {
   return (
     <Styled.h3>
-      {`${NL_NAME} is running on port ${NL_PORT} inside ${NL_OS} and ${NL_AP}`}
+      {`${NL_NAME} is running on port ${NL_PORT} inside a ${NL_OS} and ${NL_AP}`}
       <br />
       <br />
       <span> {`v ${NL_VERSION}`} </span>
